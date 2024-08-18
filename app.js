f22f71964133b7cdaff21be19635df480117fe02
Prajjwal <prajjwalkamboj05@gmail.com>
2024-08-18 23:56:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;//playerX, playerO
+let count = 0; //to track draw
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnO) {
+            box.innerText = "O"
+            turnO = false;
+        } else {
+              box.innerText = "X"
+              turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+};
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `${winner} WON `;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText
+        let pos2Val = boxes[pattern[1]].innerText
+        let pos3Val = boxes[pattern[2]].innerText
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+/dark-mode/ 
+
+const body = document.querySelector('body');
+const btn = document.querySelector('.btn');
+const icon = document.querySelector('.btn__icon');
+
+//to save the dark mode use the object "local storage".
+
+//function that stores the value true if the dark mode is activated or false if it's not.
+function store(value){
+  localStorage.setItem('darkmode', value);
+}
+
+//function that indicates if the "darkmode" property exists. It loads the page as we had left it.
+function load(){
+  const darkmode = localStorage.getItem('darkmode');
+
+  //if the dark mode was never activated
+  if(!darkmode){
+    store(false);
+    icon.classList.add('fa-sun');
+  } else if( darkmode == 'true'){ //if the dark mode is activated
+    body.classList.add('darkmode');
+    icon.classList.add('fa-moon');
+  } else if(darkmode == 'false'){ //if the dark mode exists but is disabled
+    icon.classList.add('fa-sun');
+  }
+}
+
+
+load();
+
+btn.addEventListener('click', () => {
+
+  body.classList.toggle('darkmode');
+  icon.classList.add('animated');
+
+  //save true or false
+  store(body.classList.contains('darkmode'));
+
+  if(body.classList.contains('darkmode')){
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  }else{
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
+
+  setTimeout( () => {
+    icon.classList.remove('animated');
+  }, 500)
+})
